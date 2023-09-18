de4d035e8b7721172b27ce3ab64bcf4d4b283d86
Lirys <iamyourmarkieboo@gmail.com>
2023-09-18 20:41:07 +0800

    Finished task

---
+++ JS/tally-animation.js
@@ -0,0 +1,47 @@
+let tally = document.querySelector(".tally");
+let arrowRight = document.querySelector(".right-triangle");
+let arrowLeft = document.querySelector(".left-triangle");
+
+tally.addEventListener("mouseenter", tallyEnter);
+tally.addEventListener("mouseleave", tallyOut);
+tally.addEventListener("click", formSubmit);
+
+function tallyEnter(){
+    tally.style.animation = "3s expand ease-out";
+    tally.style.height = "400px";
+    arrowRight.style.animation = "3s follow-down ease-out";
+    arrowLeft.style.animation = "3s follow-down ease-out";
+    arrowRight.style.top = "48vh";
+    arrowLeft.style.top = "48vh";
+}
+
+function tallyOut(){
+    setTimeout(() =>{
+        tally.style.animation = "2s contract ease-in";
+        arrowRight.style.animation = "2s follow-up ease-in";
+        arrowLeft.style.animation = "2s follow-up ease-in";
+        tally.style.height = "281px";
+        arrowRight.style.top = "33vh";
+        arrowLeft.style.top = "33vh";
+    }, 5000);
+}
+
+function formSubmit(e){
+    e.preventDefault();
+    let date1 = document.querySelector("#loan-start").value;
+    let date2 = document.querySelector("#loan-end").value;
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "../operations/tally-date.php", true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send("date1="+date1+"&date2="+date2);
+    xhr.onload = function () {
+        if(xhr.getResponseHeader("content-type") === "application/json"){
+            let obj = JSON.parse(xhr.responseText);
+            document.querySelector("#output-1").innerHTML = obj.day;
+            document.querySelector("#output-2").innerHTML = obj.month;
+            document.querySelector("#output-3").innerHTML = obj.year;
+        }else{
+            console.log(xhr.responseText);
+        }
+    }
+}
--- operations/tally-date.php
+++ operations/tally-date.php
@@ -1,1 +1,20 @@
 <?php
+
+if(isset($_POST["date1"], $_POST["date2"])){
+    $date1 = date_create($_POST["date1"]);
+    $date2 = date_create($_POST["date2"]);
+    $diff = date_diff($date1,$date2);
+    $year = $diff->format("%r%y");
+    $month = $diff->format("%r%m");
+    $day = $diff->format("%r%d");
+    $y = ((int) $year < 0) ? $year : 0;
+    $m = ((int) $month < 0) ? $month : 0;
+    $d = ( (int)$day < 0) ? $day : 0;
+    if($y > 0 || $m > 0 || $d > 0){
+        echo "not due";
+    }else{
+        $json = ["year" => abs($y), "month" => abs($m), "day" => abs($d)];
+        header("content-type: application/json");
+        echo json_encode($json);
+    }
+}
